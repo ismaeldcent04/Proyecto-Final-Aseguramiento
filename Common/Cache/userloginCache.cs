a639b073d27de8387c2937b63e6be4e868592919
ismaeldcent04 <87149086+ismaeldcent04@users.noreply.github.com>
2022-10-11 05:20:19 -0400

    lo que sea

---
+++ Common/Cache/userloginCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Cache
+{
+    public static class userloginCache
+    {
+        public static int id_user { get; set; }
+
+
+    }
+}
